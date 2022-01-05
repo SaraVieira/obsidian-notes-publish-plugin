e4069780996058789ba5d1448a6b64c5f82cb640
Sara Vieira <sara.vieira@remote.com>
2022-01-06 00:04:06 +0100

    remove most boilerplate

--- main.ts
+++ main.ts
@@ -1,33 +1,83 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import Airtable from 'airtable'
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ShareLinkPluginSettings {
+	airtableAPIKey: string;
+	airtableBase: string;
+	websiteUrl: string;
+	airtableView: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ShareLinkPluginSettings = {
+	airtableAPIKey: '',
+	airtableBase: '',
+	airtableView: '',
+	websiteUrl: ''
+
 }
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Airtable API Key')
+			.setDesc('This is not shared and it will stay local to your computer.')
+			.addText(text => text
+				.setPlaceholder('Enter your api key')
+				.setValue(this.plugin.settings.airtableAPIKey)
+				.onChange(async (value) => {
+					this.plugin.settings.airtableAPIKey = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Website URL')
+			.setDesc('Where your website deployed')
+			.addText(text => text
+				.setValue(this.plugin.settings.websiteUrl)
+				.onChange(async (value) => {
+					this.plugin.settings.websiteUrl = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Airtable Base ID')
+			.setDesc('What is the base ID of your Airtable base')
+			.addText(text => text
+				.setValue(this.plugin.settings.airtableBase)
+				.onChange(async (value) => {
+					this.plugin.settings.airtableBase = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Airtable View')
+			.setDesc('What the name of the view you want to use')
+			.addText(text => text
+				.setValue(this.plugin.settings.airtableView)
+				.onChange(async (value) => {
+					this.plugin.settings.airtableView = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
+
+
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: ShareLinkPluginSettings;
+	base: any;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
+		this.base = new Airtable({ apiKey: this.settings.airtableAPIKey }).base(this.settings.airtableBase);
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
@@ -41,45 +91,99 @@ export default class MyPlugin extends Plugin {
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const { airtableAPIKey, airtableBase, airtableView } = this.settings;
+				if (!airtableAPIKey || !airtableBase || !airtableView) {
+					new Notice('You need to set your Airtable API Key and Base ID in the settings tab.');
+					return
 				}
+
+				const contents = editor.getValue();
+				const { path, basename } = view.file;
+
+				this.base(airtableView).select({
+					maxRecords: 1,
+					filterByFormula: `{slug} = '${this.slugify(path)}'`
+				}).eachPage(async (records: any[]) => {
+					if (!records.length) {
+						await this.createAirtableRecord(contents, path, basename)
+						return
+					}
+					const id = records[0].getId()
+					this.base(airtableView).update([
+						{
+							"id": id,
+							"fields": {
+								"slug": this.slugify(path),
+								"data": contents,
+								"title": basename
+							}
+						}
+					], function (err: string, records: any[]) {
+						if (err) {
+							new Notice('There was an error updating your site. Please try again.');
+							return;
+						}
+						records.forEach(function (record) {
+							console.log(record.getId());
+						});
+					});
+
+				}, (err: any) => {
+					if (err) { new Notice('There has been a problem publishing your site'); return; }
+				});
+				editor.replaceSelection('Sample Editor Command');
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
+	}
+
+	createAirtableRecord(contents: string, path: string, title: string) {
+		const { airtableView, websiteUrl } = this.settings;
+		this.base(airtableView).create([
+			{
+				"fields": {
+					"slug": this.slugify(path),
+					"data": contents,
+					"title": title
+				}
+			}
+		], function (err: string, records: any[]) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			records.forEach(function (record) {
+				new Notice(websiteUrl);
+			});
+		});
+	}
+
+	slugify(str: string) {
+		str = str.replace(/^\s+|\s+$/g, ""); // trim
+		str = str.toLowerCase();
+
+		// remove accents, swap ñ for n, etc
+		var from = "åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+		var to = "aaaaaaeeeeiiiioooouuuunc------";
+
+		for (var i = 0, l = from.length; i < l; i++) {
+			str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+		}
+
+		str = str
+			.replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+			.replace(/\s+/g, "-") // collapse whitespace and replace by -
+			.replace(/-+/g, "-") // collapse dashes
+			.replace(/^-+/, "") // trim - from start of text
+			.replace(/-+$/, ""); // trim - from end of text
+
+		return str;
 	}
 
 	async loadSettings() {
@@ -97,41 +201,13 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
